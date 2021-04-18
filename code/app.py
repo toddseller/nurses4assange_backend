cfb1e53a68fb53eaaa33431ceadb383bfb0a61d6
Todd Seller <me@toddseller.com>
2021-04-18 16:04:19 -0700

    Remove unnecessary lines of code

--- code/app.py
+++ code/app.py
@@ -12,8 +12,6 @@ from security import authenticate, identity
 
 
 load_dotenv()
-# env_path = Path('.')/'.env'
-# load_dotenv(dotenv_path=env_path)
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
