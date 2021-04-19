f219ebcd392c6e51a9a9dee3a338f6920c904b96
Todd Seller <me@toddseller.com>
2021-04-18 17:26:56 -0700

    Remove / from routes

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ from security import authenticate, identity
 load_dotenv()
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv(
     'DATABASE_URL', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = os.getenv('SECRET_KEY')
@@ -28,11 +28,11 @@ def create_tables():
 
 jwt = JWT(app, authenticate, identity)
 
-api.add_resource(Item, '/item/<string:name>')
-api.add_resource(ItemList, '/items')
-api.add_resource(Store, '/store/<string:name>')
-api.add_resource(StoreList, '/stores')
-api.add_resource(UserRegister, '/register')
+api.add_resource(Item, 'item/<string:name>')
+api.add_resource(ItemList, 'items')
+api.add_resource(Store, 'store/<string:name>')
+api.add_resource(StoreList, 'stores')
+api.add_resource(UserRegister, 'register')
 
 if __name__ == '__main__':
     db.init_app(app)
