ff14dd5794363ec6be49696e7d747299e91fb6d3
Todd Seller <me@toddseller.com>
2021-04-18 17:19:55 -0700

    Setup app to use PostgreSQL on Heroku

--- app.py
+++ app.py
@@ -14,7 +14,8 @@ from security import authenticate, identity
 load_dotenv()
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv(
+    'DATABASE_URL', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = os.getenv('SECRET_KEY')
 api = Api(app)
