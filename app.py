c35ead95d129db647813323db3a96e87c5198422
Todd Seller <me@toddseller.com>
2021-05-16 18:24:35 -0700

    Update for DigitalOcean

--- app.py
+++ app.py
@@ -23,4 +23,4 @@ api.add_resource(SignatoryList, '/signatories')
 
 if __name__ == '__main__':
     db.init_app(app)
-    app.run(debug=True)
+    app.run(host='0.0.0.0')
--- run.py
+++ run.py
@@ -7,3 +7,7 @@ db.init_app(app)
 @app.before_first_request
 def create_tables():
     db.create_all()
+
+if __name__ == "__main__":
+    app.run()
+    
